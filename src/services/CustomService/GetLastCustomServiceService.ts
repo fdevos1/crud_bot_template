0b6af7537cd4050780f30c31bf9b0b2cd657ac0e
fdevos1 <felipe.devosm@gmail.com>
2022-11-10 15:58:47 -0300

    changes in service

--- src/services/CustomService/GetLastCustomServiceService.ts
+++ src/services/CustomService/GetLastCustomServiceService.ts
@@ -2,13 +2,12 @@ import { prismaClient } from "../../prisma/PrismaClient";
 
 export class GetLastCustomServiceService {
   async execute(user_cellphone: string) {
-    const customService = await prismaClient.customService.findFirst({
+    const lastService = await prismaClient.customService.findFirst({
       where: {
         user_cellphone,
-        finished_at: null,
       },
     });
 
-    return customService;
+    return lastService;
   }
 }
