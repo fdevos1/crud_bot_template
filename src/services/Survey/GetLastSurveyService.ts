85110eb4b8db2c1f400c2aa2d28b624406d480f1
fdevos1 <felipe.devosm@gmail.com>
2022-11-18 10:15:36 -0300

    returning just one item on req

--- src/services/Survey/GetLastSurveyService.ts
+++ src/services/Survey/GetLastSurveyService.ts
@@ -2,7 +2,7 @@ import { prismaClient } from "../../prisma/PrismaClient";
 
 export class GetLastSurveyService {
   async execute() {
-    const survey = await prismaClient.survey.findMany({
+    const survey = await prismaClient.survey.findFirst({
       orderBy: [
         {
           survey_id: "desc",
