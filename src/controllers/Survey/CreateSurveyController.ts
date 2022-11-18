ab4ff23ab2c2a9773bae3b21ac2f0e4a676a7374
fdevos1 <felipe.devosm@gmail.com>
2022-11-18 13:03:12 -0300

    survey send hour

--- src/controllers/Survey/CreateSurveyController.ts
+++ src/controllers/Survey/CreateSurveyController.ts
@@ -4,13 +4,14 @@ import { CreateSurveyService } from "../../services/Survey/CreateSurveyService";
 
 export class CreateSurveyController {
   async handle(req: Request, res: Response) {
-    const { survey_text, survey_subject } = req.body;
+    const { survey_text, survey_subject, survey_send_hour } = req.body;
 
     const createSurveyService = new CreateSurveyService();
 
     const response = await createSurveyService.execute(
       survey_text,
-      survey_subject
+      survey_subject,
+      survey_send_hour
     );
 
     return res.json(response);
--- src/services/Survey/CreateSurveyService.ts
+++ src/services/Survey/CreateSurveyService.ts
@@ -1,11 +1,16 @@
 import { prismaClient } from "../../prisma/PrismaClient";
 
 export class CreateSurveyService {
-  async execute(survey_text: string, survey_subject: string) {
+  async execute(
+    survey_text: string,
+    survey_subject: string,
+    survey_send_hour: Date
+  ) {
     const survey = await prismaClient.survey.create({
       data: {
         survey_text,
         survey_subject,
+        survey_send_hour,
       },
     });
 
