09a9a61d3424ff6636bea1df8c52fba213279603
fdevos1 <felipe.devosm@gmail.com>
2022-11-22 11:23:17 -0300

    findmany on service

--- src/services/Survey/GetSurveyAnswersService.ts
+++ src/services/Survey/GetSurveyAnswersService.ts
@@ -2,7 +2,7 @@ import { prismaClient } from "../../prisma/PrismaClient";
 
 export class GetSurveyAnswerService {
   async execute(survey_id: string) {
-    const surveyAnswer = await prismaClient.surveyAnswers.findFirst({
+    const surveyAnswer = await prismaClient.surveyAnswers.findMany({
       where: {
         id_from_survey: Number(survey_id),
       },
