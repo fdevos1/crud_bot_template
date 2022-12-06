5765ffce0faf1272a9bb14b4ad7618c6f41b97f8
fdevos1 <felipe.devosm@gmail.com>
2022-12-06 10:21:39 -0300

    parse int

--- src/services/Survey/CreateSurveyVoteService.ts
+++ src/services/Survey/CreateSurveyVoteService.ts
@@ -1,18 +1,18 @@
 import { prismaClient } from "../../prisma/PrismaClient";
 
 export class CreateSurveyVoteService {
-  async execute(survey_id: number, user_id: number) {
+  async execute(survey_id: string, user_id: string) {
     const survey_vote = await prismaClient.surveyVotes.findMany({
       where: {
-        answer_survey_id: survey_id,
+        answer_survey_id: parseInt(survey_id),
       },
     });
 
     if (!survey_vote) {
       const create_survey_vote = await prismaClient.surveyVotes.create({
         data: {
-          answer_survey_id: survey_id,
-          user_id_vote: user_id,
+          answer_survey_id: parseInt(survey_id),
+          user_id_vote: parseInt(user_id),
         },
       });
 
