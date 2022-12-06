571c93dd837b98a75af8dcd604c26f89c4f14fa0
fdevos1 <felipe.devosm@gmail.com>
2022-12-06 10:16:17 -0300

    changes in surveyvotes

--- src/services/Survey/CreateSurveyVoteService.ts
+++ src/services/Survey/CreateSurveyVoteService.ts
@@ -2,13 +2,21 @@ import { prismaClient } from "../../prisma/PrismaClient";
 
 export class CreateSurveyVoteService {
   async execute(survey_id: number, user_id: number) {
-    const survey_vote = await prismaClient.surveyVotes.create({
-      data: {
+    const survey_vote = await prismaClient.surveyVotes.findMany({
+      where: {
         answer_survey_id: survey_id,
-        user_id_vote: user_id,
       },
     });
 
-    return survey_vote;
+    if (!survey_vote) {
+      const create_survey_vote = await prismaClient.surveyVotes.create({
+        data: {
+          answer_survey_id: survey_id,
+          user_id_vote: user_id,
+        },
+      });
+
+      return create_survey_vote;
+    }
   }
 }
