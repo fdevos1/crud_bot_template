60967f00d4fa7af299d984b9e4eaf5144550df33
fdevos1 <felipe.devosm@gmail.com>
2022-12-06 10:33:15 -0300

    console log to search error

--- src/controllers/Survey/CreateSurveyVoteController.ts
+++ src/controllers/Survey/CreateSurveyVoteController.ts
@@ -5,9 +5,13 @@ export class CreateSurveyVoteController {
   async handle(req: Request, res: Response) {
     const { survey_answer_id, user_id } = req.body;
 
+    console.log(survey_answer_id, user_id);
+
     const service = new CreateSurveyVoteService();
 
     const result = await service.execute(survey_answer_id, user_id);
+
+    console.log(result);
 
     return res.json(result);
   }
