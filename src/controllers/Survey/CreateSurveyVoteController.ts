4e0996e4affa578576f6b2ba7e9378e20140b8d1
fdevos1 <felipe.devosm@gmail.com>
2023-01-02 07:51:00 -0300

    remove console.log

--- src/controllers/Survey/CreateSurveyVoteController.ts
+++ src/controllers/Survey/CreateSurveyVoteController.ts
@@ -5,8 +5,6 @@ export class CreateSurveyVoteController {
   async handle(req: Request, res: Response) {
     const { survey_id, user_id_vote, answer_text } = req.body;
 
-    console.log(survey_id, user_id_vote);
-
     const service = new CreateSurveyVoteService();
 
     const result = await service.execute(survey_id, user_id_vote, answer_text);
